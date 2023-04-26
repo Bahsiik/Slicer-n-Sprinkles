2e06d433e4caef9f3a7bfc5f867a6236a86e6876
Ayfri <pierre.ayfri@gmail.com>
2023-04-26 13:48:34 +0200

    feat(object thrower): Add randomized launch position on X axis for object thrower.

--- Assets/Scripts/Level/ObjectThrower.cs
+++ Assets/Scripts/Level/ObjectThrower.cs
@@ -1,33 +1,29 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectThrower : MonoBehaviour
-{
+public class ObjectThrower : MonoBehaviour {
 	public List<GameObject> objectsToThrow;
-	public float throwForce = 10f;
+	public float throwForce = 20f;
 	public float throwDelay = 1f;
+	public int randomLaunchPositionXStart = -7;
+	public int randomLaunchPositionXEnd = 7;
 
 	private int _currentObjectIndex;
-	// private Timer _timer;
 
-	private void Start()
-	{
-		/*_timer = new(throwDelay * 1000);
-		_timer.Elapsed += OnTimerElapsed;
-		_timer.Start();*/
-
+	private void Start() {
 		InvokeRepeating(nameof(OnTimerElapsed), 0, throwDelay);
 	}
 
-	private void OnTimerElapsed()
-	{
-		Debug.Log("Throwing object");
+	private void OnTimerElapsed() {
+		var position = transform.position;
 		var prefab = objectsToThrow[_currentObjectIndex];
-		Debug.Log(prefab);
-		var obj = Instantiate(prefab, transform.position, Quaternion.identity);
+		var launchPosition = new Vector3(Random.Range(randomLaunchPositionXStart, randomLaunchPositionXEnd), position.y, position.z);
+
+		var obj = Instantiate(prefab, launchPosition, Quaternion.identity);
 		var rb = obj.GetComponent<Rigidbody>();
 		rb.useGravity = true;
 		rb.AddForce(transform.forward * throwForce, ForceMode.Impulse);
+
 		_currentObjectIndex = (_currentObjectIndex + 1) % objectsToThrow.Count;
 	}
 }
