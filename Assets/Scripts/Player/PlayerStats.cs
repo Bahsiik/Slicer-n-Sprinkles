715ee423c0a2c1e9d093912dde98b4ca76a16af4
Ayfri <pierre.ayfri@gmail.com>
2023-04-29 01:53:25 +0200

    feat(points): Add play board points counter, add points icon, add helper to update text & document how it can work with unity serialization.

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,9 +1,33 @@
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour
-{
+public class PlayerStats : MonoBehaviour {
+	public TextUpdater pointsText;
+
 	public int lives = 5;
 
-	public int points;
+	[SerializeField]
+	private int points;
+
+	public int Points {
+		get => points;
+		set {
+			points = value;
+			pointsText.UpdateText(points);
+		}
+	}
+
+	private void Awake() {
+		Points = 0;
+	}
+
+	/// <summary>
+	///     This method is called when the script is loaded or a value is changed in the inspector (Called in the editor only).
+	///     We use this trick to update the points text in the editor as it will call the setter of the Points property.
+	/// </summary>
+	private void OnValidate() {
+		if (!pointsText.IsAwake) return;
+		Points = points;
+	}
+
 	public bool IsDead() => lives <= 0;
 }
