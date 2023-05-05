26f439c9bee25a8d4f9b65fd2c24f764a0cab73f
Ayfri <pierre.ayfri@gmail.com>
2023-05-05 18:00:49 +0200

    fix(ingredients): Remove collisions between ingredients.
    Closes #38

--- Assets/Scripts/Level/ObjectsCollision.cs
+++ Assets/Scripts/Level/ObjectsCollision.cs
@@ -1,20 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectsCollision : MonoBehaviour
-{
-	private void OnCollisionEnter(Collision other)
-	{
-		// if (!other.gameObject.CompareTag("Player"))
-		// {
-		// 	Physics.IgnoreCollision(other.collider, GetComponent<Collider>());
-		// 	Debug.Log("Ignore collision with " + other.gameObject.name);
-		// }
-		
-		if (other.gameObject.CompareTag("Ingredient") || other.gameObject.CompareTag("Untagged"))
-		{
-			Physics.IgnoreCollision(other.collider, GetComponent<Collider>());
-		}
-	}
-}
+public class ObjectsCollision : MonoBehaviour { }
