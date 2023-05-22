a5a3f9bb6fa754573b6f06804fb2bf1fba6b0ac4
Ayfri <pierre.ayfri@gmail.com>
2023-05-22 17:55:27 +0200

    feat(gameover): Display new high score text when beating the highest current score.
    Closes #62

--- Assets/Scripts/GameOverMenu/GameOver.cs
+++ Assets/Scripts/GameOverMenu/GameOver.cs
@@ -1,4 +1,5 @@
-﻿using PauseMenu;
+﻿using System.Linq;
+using PauseMenu;
 using Player;
 using TMPro;
 using UnityEngine;
@@ -10,11 +11,13 @@ namespace GameOverMenu
 		private static bool _isDead;
 		private static GameObject _gameOverPanel;
 		private static TextMeshProUGUI _gameOverText;
+		private static TextMeshProUGUI _highScoreText;
 
 		public void Start()
 		{
 			_gameOverPanel = GameObject.Find("GameOverCanvas");
 			_gameOverText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+			_highScoreText = GameObject.Find("NewHighScoreText").GetComponent<TextMeshProUGUI>();
 			_gameOverPanel.SetActive(false);
 			Time.timeScale = 1f;
 			_isDead = false;
@@ -25,10 +28,16 @@ namespace GameOverMenu
 			if (!_isDead) _isDead = true;
 			else return;
 
+			var highScore = SaveGame.LoadAllData().DefaultIfEmpty(new(0, 0)).Max(static x => x.score);
+
 			SaveGame.SaveIntoLatestSlot();
 
 			_gameOverText.text = string.Format(_gameOverText.text, PlayerStats.Instance.Points);
 			_gameOverPanel.SetActive(!_gameOverPanel.activeSelf);
+
+			if (PlayerStats.Instance.Points <= highScore) _highScoreText.gameObject.SetActive(false);
+			else _highScoreText.gameObject.GetComponent<Animator>().Play("NewHighScoreAnimation", -1, 0f);
+
 			Time.timeScale = _gameOverPanel.activeSelf ? 0f : 1f;
 			TogglePauseMenu.isPaused = true;
 		}
